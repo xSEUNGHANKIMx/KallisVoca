f8896b13ec6eafbeb95a281604306258e115c088
Seung Han Kim <ccomett@gmail.com>
2017-10-02 16:42:54 -0700

    initial working with basic function.

--- app/src/main/java/com/kallis/satvocabulary/MainActivity.java
+++ app/src/main/java/com/kallis/satvocabulary/MainActivity.java
@@ -1,13 +1,48 @@
 package com.kallis.satvocabulary;
 
+import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.io.IOException;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.recycler_view)
+    protected RecyclerView mRecyclerView;
+
+    @Bind(R.id.toolbar)
+    protected Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setSupportActionBar(mToolbar);
+
+        String[] dataSet = null;
+        try {
+            dataSet = getAssets().list("demo-pictures");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PhotoAdapter adapter = new PhotoAdapter(dataSet, this);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                outRect.bottom = getResources().getDimensionPixelSize(R.dimen.activity_vertical_margin);
+            }
+        });
+        mRecyclerView.setAdapter(adapter);
     }
 }
