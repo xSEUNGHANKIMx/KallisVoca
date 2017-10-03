5abbbbddd826e60bdf9d9d0c3e0894bd98282303
Seung Han Kim <ccomett@gmail.com>
2017-10-03 11:38:19 -0700

    Add Search action on ActionBar

--- app/src/main/java/com/kallis/satvocabulary/MainActivity.java
+++ app/src/main/java/com/kallis/satvocabulary/MainActivity.java
@@ -1,11 +1,16 @@
 package com.kallis.satvocabulary;
 
 import android.graphics.Rect;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 import java.io.IOException;
@@ -44,5 +49,28 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         mRecyclerView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                searchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
